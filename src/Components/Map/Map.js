4c9ccb45b8dbdfb5cdd18c84fc09dffbd25f73c4
syket-git <syketb@gmail.com>
2021-03-30 23:44:08 +0600

    google map added

--- src/Components/Map/Map.js
+++ src/Components/Map/Map.js
@@ -1,21 +1,44 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import GoogleMapReact from 'google-map-react';
+import './Map.css';
+import useOnClickOutside from '../useOnclickOutside/useOnclickOutside';
 require('dotenv').config();
 
-const AnyReactComponent = ({ text }) => (
-  <div
-    style={{
-      fontSize: '16px',
-      backgroundColor: 'black',
-      color: 'white',
-      width: 'fit-content',
-      padding: '5px',
-      borderRadius: '30px',
-    }}
-  >
-    ${text}
-  </div>
-);
+const AnyReactComponent = ({ text, data }) => {
+  const ref = useRef();
+  const [click, setClick] = useState(false);
+  useOnClickOutside(ref, () => setClick(false));
+  return (
+    <div className="marker__wrapper">
+      {click && (
+        <div ref={ref} className="marker__info">
+          <p>{data.title}</p>
+
+          <div className="d-flex align-items-center justify-content-between">
+            <span>
+              <span
+                style={{
+                  color: 'goldenrod',
+                  fontSize: '22px',
+                  marginRight: '10px',
+                }}
+              >
+                &#9733;
+              </span>
+              {data.ratings}/5
+            </span>
+            <span>
+              <span className="font-weight-bold">${data.amount}</span>/night
+            </span>
+          </div>
+        </div>
+      )}
+      <div onClick={() => setClick(!click)} className="marker">
+        ${text}
+      </div>
+    </div>
+  );
+};
 
 const Map = ({ data, center }) => {
   return (
@@ -33,11 +56,13 @@ const Map = ({ data, center }) => {
           defaultCenter={center}
           defaultZoom={17}
         >
-          {data.map((dt) => (
+          {data.map((dt, i) => (
             <AnyReactComponent
+              key={i}
               lat={dt.location.lat}
               lng={dt.location.lng}
               text={dt.amount}
+              data={dt}
             />
           ))}
         </GoogleMapReact>
