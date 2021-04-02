235d78dbcf7175a0a8e38fd9dc7c9d72361d5b09
Syket Bhattachergee <39830305+syket-git@users.noreply.github.com>
2021-04-02 12:49:57 +0600

    Delete firebase.js

--- src/firebase.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: 'AIzaSyA6-8R4e0qN8ZRrxFlrZWuk23VRcLdWvlA',
-  authDomain: 'travel-guru-99.firebaseapp.com',
-  projectId: 'travel-guru-99',
-  storageBucket: 'travel-guru-99.appspot.com',
-  messagingSenderId: '27300642948',
-  appId: '1:27300642948:web:0e1036f3f96636a647c2d1',
-};
